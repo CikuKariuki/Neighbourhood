fe249b48462033161ff49d6350393a5d6bf4c9d1
Wanjiku <Wanjiku>
2019-06-03 09:41:39 +0300

    Created views for the search function

--- neighbours/views.py
+++ neighbours/views.py
@@ -6,4 +6,17 @@ from django.shortcuts import render,HttpResponse
 def index(request):
     return render(request,'hood.html')
 
+def search_results(request):
+    if 'business' in request.GET and request.GET['business']:
+        search_term = request.GET.get("business")
+        searched_business = Business.search_by_business(search_term)
 
+        message = f'{search_term}'
+
+        return render(request,'search.html',{"message":message,"business":searched_business})
+
+    else:
+        message = "Invalid input"
+        return render(request, 'search.html',{"message":message,"business":searched_business})
+
+        
