7fb20a42f653805e764c29edd7a47a04e121e4b9
Wanjiku <Wanjiku>
2019-06-03 08:55:40 +0300

    changed url for login and registration

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('neighbours.urls')),
+    # url(r'',include('neighbours.urls')),
     url(r'^accounts/',include('registration.backends.simple.urls')),
     
 ]
