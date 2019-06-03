60d8c1e551a3c8b5f934a96f06846cd7f03942b7
Wanjiku <Wanjiku>
2019-06-03 10:43:27 +0300

    defined url for search

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls,name=admin),
     url(r'',include('neighbours.urls')),
     url(r'^accounts/',include('registration.backends.simple.urls')),
     
--- neighbours/urls.py
+++ neighbours/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$',views.index,name='index'), 
+     url(r'^search/',views.search_results,name='search_results'),
 ]
