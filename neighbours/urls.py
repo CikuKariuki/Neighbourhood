e2b36d760eac5b32288a7307a976b3de46e4ac85
Wanjiku <Wanjiku>
2019-06-03 09:22:37 +0300

    Created database, linked via settings, included bootstrap and neighbours app to installed apps

--- neighbours/urls.py
+++ neighbours/urls.py
@@ -1,8 +1,8 @@
-# from django.conf.urls import url
-# from . import views
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-# urlpatterns = [
-    
-# ]
+urlpatterns = [
+    url('^$',views.index,name='index'), 
+]
--- neighbours/views.py
+++ neighbours/views.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+
+
+def index(request):
+
+    return HttpResponse('test')
+
 
